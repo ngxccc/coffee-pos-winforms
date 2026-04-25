cf0942ed1da7c099a3105c6014945441a7e0de72
ngxc <ngoc.shintrann@gmail.com>
2026-04-25 12:56:23 +0700

    feat(DbInitializer): add 'XL' size option to product_size enum

--- src/Migrator/Sql/DbInitializer/001_create_enums.sql
+++ src/Migrator/Sql/DbInitializer/001_create_enums.sql
@@ -4,7 +4,7 @@ DO $$
 DECLARE sql_cmd text;
 BEGIN -- HACK: Using specific dollar tags ($cmd$) prevents quote-escaping hell for the internal strings
 FOREACH sql_cmd IN ARRAY ARRAY [
-        $cmd$ CREATE TYPE product_size AS ENUM ('S', 'M', 'L') $cmd$,
+        $cmd$ CREATE TYPE product_size AS ENUM ('S', 'M', 'L', 'XL') $cmd$,
         $cmd$ CREATE TYPE bill_order_type AS ENUM ('dine_in', 'takeaway') $cmd$,
         $cmd$ CREATE TYPE bill_status AS ENUM ('pending', 'paid', 'canceled') $cmd$,
         $cmd$ CREATE TYPE user_role AS ENUM ('admin', 'cashier') $cmd$
