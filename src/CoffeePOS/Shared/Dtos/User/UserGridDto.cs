a0f52994cc8d34394dcb8a482fb9a1d126aeaa2e
ngxc <ngoc.shintrann@gmail.com>
2026-04-25 09:29:12 +0700

    fix(dto): update display name for ID property in UserGridDto

--- src/CoffeePOS/Shared/Dtos/User/UserGridDto.cs
+++ src/CoffeePOS/Shared/Dtos/User/UserGridDto.cs
@@ -4,7 +4,7 @@ using CoffeePOS.Shared.Enums;
 namespace CoffeePOS.Shared.Dtos.User;
 
 public record UserGridDto(
-    [property: DisplayName("Mã")] int Id,
+    [property: DisplayName("ID")] int Id,
     [property: DisplayName("Tài khoản")] string Username,
     [property: DisplayName("Họ tên")] string FullName,
     [property: Browsable(false)] UserRole Role,
